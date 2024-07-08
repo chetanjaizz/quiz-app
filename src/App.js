97cd4ac63c22f796747171a63ea9f711748526af
chetanjaizz <chetanjaiswal2306@gmail.com>
2024-07-08 20:25:17 +0530

    Initial commit: Added React project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import QuestionScreen from './components/QuestionScreen';
+import ScoreScreen from './components/ScoreScreen';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center style = {{marginTop:"20%"}}>
+      
+    <Router>
+      <Routes>
+        <Route path="/" element={<QuestionScreen />} />
+        <Route path="/score" element={<ScoreScreen />} />
+      </Routes>
+    </Router>
+    </center>
   );
 }
 
